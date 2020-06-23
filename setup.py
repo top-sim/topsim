52793e137875e37e3018f1a58af43f71934bfed8
myxie <21298244@student.uwa.edu.au>
2020-06-23 11:52:09 +0800

    Setup for binder

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='topsim',
+	version='0.1',
+	packages=find_packages(),
+	# package_dir={'': 'shadow'},
+	url='https://github.com/myxie/topsim',
+	license='GNU',
+	author='Ryan Bunney',
+	author_email='ryan.bunney@icrar.org',
+	description=''
+)
