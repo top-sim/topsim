afd216f7bef64f423bcdb883ad3e143a8c1dd3f2
myxie <21298244@student.uwa.edu.au>
2020-06-24 12:56:38 +0800

    add postBuild for proxy

--- bokehserverextension.py
+++ bokehserverextension.py
@@ -12,3 +12,32 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# Adapted from https://github.com/binder-examples/bokeh/blob/master/bokehserverextension.py
+import os
+import urllib.parse
+
+
+def remote_jupyter_proxy_url(port):
+    """
+    Callable to configure Bokeh's show method when a proxy must be
+    configured.
+
+    If port is None we're asking about the URL
+    for the origin header.
+    """
+    base_url = os.environ['EXTERNAL_URL']
+
+    host = urllib.parse.urlparse(base_url).netloc
+
+    # If port is None we're asking for the URL origin
+    # so return the public hostname.
+    if port is None:
+        return host
+
+    service_url_path = os.environ['JUPYTERHUB_SERVICE_PREFIX']
+    proxy_url_path = 'proxy/%d' % port
+
+    user_url = urllib.parse.urljoin(base_url, service_url_path)
+    full_url = urllib.parse.urljoin(user_url, proxy_url_path)
+
+    return full_url
