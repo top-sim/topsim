29e20784455ee105b829e927e9697fe06d79e30e
myxie <21298244@student.uwa.edu.au>
2020-09-01 18:22:43 +0800

    New 'recipes' document that runs a simple simulation

---
+++ recipes/default.py
@@ -0,0 +1,25 @@
+# Copyright (C) 1/9/20 RW Bunney
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+This script runs a default simulation, with no 'bells and whistles'.
+It uses a standard scheduling and planning model with no failures,
+and a homogneoues cluster system.
+
+The only requirements are the SHADOW library implementations of static
+scheduling algorithms for the Planner interface, Simpy, and numby/matplotlib.
+"""
+
+
