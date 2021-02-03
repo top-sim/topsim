678ab622b52a118525b92895835a2ba5c4eecccd
myxie <21298244@student.uwa.edu.au>
2021-02-03 18:59:08 +0800

    Update Git repository URL

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
 	version='0.1',
 	packages=find_packages(),
 	# package_dir={'': 'shadow'},
-	url='https://github.com/myxie/topsim',
+	url='https://github.com/top-sim/topsim',
 	license='GNU',
 	author='Ryan Bunney',
 	author_email='ryan.bunney@icrar.org',
