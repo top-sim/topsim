4d98e56465b7381c4456891e94e44f8b58fdadf1
myxie <21298244@student.uwa.edu.au>
2021-02-04 15:06:50 +0800

    Update install_requires for setup and install from pip

--- setup.py
+++ setup.py
@@ -4,10 +4,21 @@ setup(
 	name='topsim',
 	version='0.1',
 	packages=find_packages(exclude=("test",)),
+	install_requires=[
+		'networkx>=2.0',
+		'matplotlib>=3.0',
+		'simpy>=3.0',
+		'pandas>=0.20',
+		'shadow @ git+https://github.com/myxie/shadow.git'
+	],
+	dependency_links=[
+		'http://github.com/myxie/shadow/tarball/master#egg=shadow'
+	],
+
 	# package_dir={'': 'shadow'},
 	url='https://github.com/top-sim/topsim',
 	license='GNU',
 	author='Ryan Bunney',
 	author_email='ryan.bunney@icrar.org',
-	description=''
+	description='Simulation framework'
 )
