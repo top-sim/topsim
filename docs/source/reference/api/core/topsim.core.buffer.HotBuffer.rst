d7ef0ad7cda0e70f28720ef41f12cdbc4ae782cc
myxie <21298244@student.uwa.edu.au>
2022-01-20 13:41:25 +0800

    Requirements.txt updated to include `tables`

---
+++ docs/source/reference/api/core/topsim.core.buffer.HotBuffer.rst
@@ -0,0 +1,26 @@
+﻿topsim.core.buffer.HotBuffer
+============================
+
+.. currentmodule:: topsim.core.buffer
+
+.. autoclass:: HotBuffer
+
+
+   
+   
+   
+
+   
+
+   
+   .. rubric:: Methods
+
+   .. autosummary::
+   
+      ~HotBuffer.__init__
+      ~HotBuffer.has_waiting_observations
+      ~HotBuffer.observation_for_transfer
+      ~HotBuffer.process_incoming_data_stream
+      ~HotBuffer.transfer_observation
+   
+   
--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='topsim',
-	version='0.1',
-	packages=find_packages(exclude=["test"]),
-	install_requires=[
-		'networkx>=2.0',
-		'matplotlib>=3.0',
-		'simpy>=3.0',
-		'pandas>=0.20',
-		'shadow @ git+https://github.com/myxie/shadow.git'
-	],
-	dependency_links=[
-		'http://github.com/myxie/shadow/tarball/master#egg=shadow'
-	],
+    name='topsim',
+    version='0.1',
+    packages=find_packages(exclude=["tests"]),
+    install_requires=[
+        'networkx>=2.0',
+        'matplotlib>=3.0',
+        'simpy>=3.0',
+        'pandas>=0.20',
+        'shadow @ git+https://github.com/myxie/shadow.git'
+    ],
+    dependency_links=[
+        'http://github.com/myxie/shadow/tarball/master#egg=shadow'
+    ],
 
-	# package_dir={'': 'shadow'},
-	url='https://github.com/top-sim/topsim',
-	license='GNU',
-	author='Ryan Bunney',
-	author_email='ryan.bunney@icrar.org',
-	description='Simulation framework'
+    # package_dir={'': 'shadow'},
+    url='https://github.com/top-sim/topsim',
+    license='GNU',
+    author='Ryan Bunney',
+    author_email='ryan.bunney@icrar.org',
+    description='Simulation framework'
 )
